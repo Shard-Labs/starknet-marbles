7ee2e8d795cba38e1f19e703e1806e8d7bba7d04
Antun Badurina <antun.badurina@gmail.com>
2022-04-25 23:16:33 +0200

    Change title

--- marbles-ui/src/pages/_app.tsx
+++ marbles-ui/src/pages/_app.tsx
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     return (
         <>
             <NextHead>
-                <title>StarkNet ❤️ React</title>
+                <title>StarkNet ❤️ Marbles</title>
             </NextHead>
             <Component {...pageProps} />
         </>

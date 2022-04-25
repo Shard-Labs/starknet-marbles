f6d55868292f131409343d0d3759c5a372720d61
Antun Badurina <antun.badurina@gmail.com>
2022-04-25 12:08:47 +0200

    Install three.js

---
+++ web/marbles-ui/src/pages/marble.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Marble: NextPage = () => {
+    return (
+        <div>
+            <h1>Marble render</h1>
+        </div>
+    )
+}
+
+export default Marble
